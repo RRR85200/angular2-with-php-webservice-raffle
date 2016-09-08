e51e04336aaed8a18cdd184ecbea7e696a30a84a
marciioluucas <marciioluucas@gmail.com>
2016-09-08 17:11:08 -0300

    Criado service http get

--- front/app/app.component.ts
+++ front/app/app.component.ts
@@ -5,5 +5,12 @@ import { Component } from '@angular/core';
     templateUrl: '../template/welcome.html'
 })
 export class AppComponent {
+  private email:String;
+  private senha:String;
+
+  verificaLogin(){
+    //TODO: implementar
+    //  +Logica: Mandar o link com as credenciais, se sim. retorna true com todas as informações do usuario
+  }
 
 }
