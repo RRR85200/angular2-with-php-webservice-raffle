51fd945c11a2b3958fb9ef6e76beb6f9002123f3
marciioluucas <marciioluucas@gmail.com>
2016-09-09 14:46:31 -0300

    Implementando metodos CRUD com retorno em json

--- back/model/Banco.php
+++ back/model/Banco.php
@@ -9,5 +9,158 @@
  */
 class Banco
 {
+    private $sql;
+    public $query;
+    public $result;
+    private $host;
+    private $usuario;
+    private $senha;
+    private $banco;
+    public $tabela;
+    public $campos;
+    public $valores;
+    public $condicao;
+    private $link;
 
+    public function __construct()
+    {
+        $this->conexao();
+    }
+
+    private function conexao()
+    {
+        $this->host = "localhost";
+        $this->banco = "bd_sorteio";
+        $this->usuario = "root";
+        $this->senha = "";
+        $this->link = mysqli_connect($this->host, $this->usuario, $this->senha, $this->banco);
+        if ($this->link) {
+            return "
+            {
+  \"banco\": {
+    \"tipo_banco\": \"MySQL\",
+    \"conectado\": true,
+    \"ipbanco\": \"$this->host\",
+    \"erro\": \"" . mysqli_error($this->link) . "\"
+    \"obs\": \"SHOW o/\"
+  }
+}";
+        } else {
+            return "
+            {
+  \"banco\": {
+    \"tipo_banco\": \"MySQL\",
+    \"conectado\": true,
+    \"ipbanco\": \"$this->host\",
+    \"erro\": \"" . mysqli_error($this->link) . "\"
+    \"obs\": \"SHOW o/\"
+  }
+}";
+
+        }
+    }
+
+    public function cadastrar()
+    {
+        if(1==1){
+            return "
+        {
+            \"origem\":{
+                \"classe\":\"banco\",
+                \"metodo\":\"cadastrar\"
+            },
+            \"msg\": \"Sucesso!\"
+        }
+        ";
+        }else{
+            return "
+        {
+            \"origem\":{
+                \"classe\":\"banco\",
+                \"metodo\":\"cadastrar\"
+            },
+            \"msg\": \"Erro!\"
+        }
+        ";
+        }
+
+    }
+
+    public function alterar()
+    {
+        if(1==1){
+            return "
+        {
+            \"origem\":{
+                \"classe\":\"banco\",
+                \"metodo\":\"alterar\"
+            },
+            \"msg\": \"Sucesso!\"
+        }
+        ";
+        }else{
+            return "
+        {
+            \"origem\":{
+                \"classe\":\"banco\",
+                \"metodo\":\"alterar\"
+            },
+            \"msg\": \"Erro!\"
+        }
+        ";
+        }
+
+    }
+
+    public function excluir()
+    {
+        if(1==1){
+            return "
+        {
+            \"origem\":{
+                \"classe\":\"banco\",
+                \"metodo\":\"excluir\"
+            },
+            \"msg\": \"Sucesso!\"
+        }
+        ";
+        }else{
+            return "
+        {
+            \"origem\":{
+                \"classe\":\"banco\",
+                \"metodo\":\"excluir\"
+            },
+            \"msg\": \"Erro!\"
+        }
+        ";
+        }
+
+    }
+
+    public function listar()
+    {
+        if(1==1){
+            return "
+        {
+            \"origem\":{
+                \"classe\":\"banco\",
+                \"metodo\":\"cadastrar\"
+            },
+            \"msg\": \"Sucesso!\"
+        }
+        ";
+        }else{
+            return "
+        {
+            \"origem\":{
+                \"classe\":\"banco\",
+                \"metodo\":\"cadastrar\"
+            },
+            \"msg\": \"Erro!\"
+        }
+        ";
+        }
+
+    }
 }
