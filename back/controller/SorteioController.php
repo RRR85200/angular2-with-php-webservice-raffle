3205be3b2a22e46f9fc7323eb59f15e273c3aab6
marciioluucas <marciioluucas@gmail.com>
2016-09-12 14:07:43 -0300

    alterando os controllers v 1.2

--- back/controller/SorteioController.php
+++ back/controller/SorteioController.php
@@ -11,11 +11,7 @@ class SorteioController
 {
     public function __construct()
     {
-        $this->usuario = new Usuario();
 
-        if (isset($_GET["entrar"]) && $_GET['entrar'] == "1") {
-            echo $this->usuario->logarUsuario($_GET['email'], $_GET['senha']);
-        }
     }
     public function cadastrar()
     {
