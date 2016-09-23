f26e7777963ffd63aaac04404d4d3925135e848f
marciioluucas <marciioluucas@gmail.com>
2016-09-23 09:41:00 -0300

    Classe endereço com os atributos do json

--- front/app/model/Endereco.ts
+++ front/app/model/Endereco.ts
@@ -2,13 +2,18 @@
  * Created by Carlos Eduardo on 23/09/2016.
  */
 export class Endereco {
-    private id: number;
-    private idUsuario: number;
-    private pais: string;
-    private estado: string;
-    private cidade: string;
-    private bairro: string;
-    private rua: string;
-    private numero: string;
-    private complemento: string;
+    // private id: number;
+    // private idUsuario: number;
+    // private pais: string;
+    // private estado: array[];
+    // private cidade: string;
+    // private bairro: string;
+    // private rua: string;
+    // private numero: string;
+    // private complemento: string;
+    estado: Endereco;
+    sigla:string;
+    nome:string;
+    cidades:string[]
+
 }
