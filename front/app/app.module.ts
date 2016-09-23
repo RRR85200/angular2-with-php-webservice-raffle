ccca66553ac8ef42aeccb3a3f4e2affb3ab1c1fd
marciioluucas <marciioluucas@gmail.com>
2016-09-23 10:01:21 -0300

    adicionando o service do endereço

--- front/app/app.module.ts
+++ front/app/app.module.ts
@@ -1,29 +1,32 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import {AppComponent}  from './app.component';
 import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
 import {HttpService} from "./services/http.service";
 import {LoginComponent} from "./view/login.component";
 import {CadastroUsuarioComponent} from "./view/cadastroUsuario.component";
+import {EnderecoService} from "./services/endereco.service";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    CadastroUsuarioComponent
-  ],
-  providers: [
-    HttpService
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        CadastroUsuarioComponent
+    ],
+    providers: [
+        HttpService,
+        EnderecoService
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule { }
+export class AppModule {
+}
