95a8681df6209df7fd1fe1cd01eae2c1303f5d1d
marciioluucas <marciioluucas@gmail.com>
2016-09-23 09:34:01 -0300

    criando classe endereço

---
+++ front/app/model/Endereco.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Carlos Eduardo on 23/09/2016.
+ */
+export class Endereco {
+    private id: number;
+    private idUsuario: number;
+    private pais: string;
+    private estado: string;
+    private cidade: string;
+    private bairro: string;
+    private rua: string;
+    private numero: string;
+    private complemento: string;
+}
